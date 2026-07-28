f455db8dd4a807312ae874fb6bab163d9957b6d1
SGErik1899 <eriik@hotmail.de>
2026-07-28 20:27:36 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
-console.log("Firebase.js wurde geladen");
+// Firebase SDK importieren
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+// Firebase-Konfiguration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxdlwpIrYzOZth3h2NMOOva2NRVVPFX0M",
+  authDomain: "reiseplaner-erik.firebaseapp.com",
+  projectId: "reiseplaner-erik",
+  storageBucket: "reiseplaner-erik.firebasestorage.app",
+  messagingSenderId: "1025613186399",
+  appId: "1:1025613186399:web:949472acc21f59b664a2ea"
+};
+
+// Firebase starten
+const app = initializeApp(firebaseConfig);
+
+console.log("✅ Firebase erfolgreich initialisiert");
